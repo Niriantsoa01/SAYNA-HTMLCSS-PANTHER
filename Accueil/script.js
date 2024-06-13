31256e5e9a0343a547efd0f7992d8a38265d487a
niriantsoaherge@gmail.com <niriantsoaherge@gmail.com>
2024-06-13 20:49:59 +0300

    modif dans accueil.html et wakanda.html

--- Accueil/script.js
+++ Accueil/script.js
@@ -1,31 +1,39 @@
-let modal = document.getElementById("myModal");
+
 let btn = document.getElementById("button");
 let span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.onclick = function(event) {
     let nom = document.getElementById('name').value;
     let email = document.getElementById('mail').value;
-    if(nom === '' || nom === null &&
-    email === '' || email === null) {
-        let errors = document.querySelectorAll('.error');
-        errors.forEach(error => {
-            error.textContent = 'Veuillez remplir ce champ';
-            error.style.color = 'red';
-        });
-    } else {
+    if(nom !== '' &&
+    email !== '' ) {
+        event.preventDefault();
+        let modal = document.getElementById("myModal");
         modal.style.display = "block";
+
+        document.getElementById('name').removeAttribute('required');
+        document.getElementById('mail').removeAttribute('required');
+
+
+
         document.getElementById('name').value = '';
         document.getElementById('mail').value = '';
-    }   
+
+    } 
 }
  
 span.onclick = function() {
-modal.style.display = "none";
+    let modal = document.getElementById("myModal");   
+    modal.style.display = "none";
+
+    document.getElementById('name').setAttribute('required', '');
+    document.getElementById('mail').setAttribute('required', '');
 }
 
 window.onclick = function(event) {
-if (event.target == modal) {
- modal.style.display = "none";
+    let modal = document.getElementById("myModal");
+    if (event.target === modal) {
+    modal.style.display = "none";
 }
 }
 
